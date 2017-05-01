53a6aea9fc8e3182782871bfe78ce9b7abf60912
lin <haipeng.lin@inspiry.cn>
2017-05-01 17:08:49 +0800

    sql

---
+++ apptest/src/main/resources/test.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `user_t` (
+   `id` int(11) NOT NULL AUTO_INCREMENT,
+   `user_name` varchar(40) NOT NULL,
+   `password` varchar(255) NOT NULL,
+   `age` int(4) NOT NULL,
+   PRIMARY KEY (`id`)
+ ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8
