337a12588d574a5355fb4c3226c4867c6797bd96
lin <haipeng.lin@inspiry.cn>
2017-06-30 00:35:00 +0800

    update code

---
+++ apptest/src/main/java/com/ssm/mqTest/Person.java
@@ -0,0 +1,7 @@
+package com.ssm.mqTest;
+
+/**
+ * Created by lhp on 2017/6/29.
+ */
+public class Person {
+}
---
+++ apptest/src/test/java/com/ssm/testmybatis/SendMQ.java
@@ -0,0 +1,7 @@
+package com.ssm.testmybatis;
+
+/**
+ * Created by lhp on 2017/6/29.
+ */
+public class SendMQ {
+}
